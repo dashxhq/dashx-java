d9d368132981ad7d2f213ef5f45c172d5f61b12b
Gulshan Dhingra <gdgulshan.007@gmail.com>
2025-04-13 20:19:13 +0530

    Expose Asset url

--- dashx/src/main/kotlin/com/dashx/graphql/models/Asset.kt
+++ dashx/src/main/kotlin/com/dashx/graphql/models/Asset.kt
@@ -4,11 +4,9 @@ import com.dashx.graphql.generated.Timestamp
 import com.dashx.graphql.generated.UUID
 import com.dashx.graphql.generated.enums.AssetProcessingStatus
 import com.dashx.graphql.generated.enums.AssetUploadStatus
-import com.dashx.graphql.generated.scalars.JsonObjectSerializer
 import kotlin.Int
 import kotlin.String
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonObject
 
 @Serializable
 public data class Asset(
@@ -18,7 +16,7 @@ public data class Asset(
         public val uploaderId: UUID? = null,
         public val name: String? = null,
         public val mimeType: String? = null,
-        @Serializable(with = JsonObjectSerializer::class) public val data: JsonObject,
+        public val url: String? = null,
         public val size: Int? = null,
         public val uploadStatus: AssetUploadStatus = AssetUploadStatus.__UNKNOWN_VALUE,
         public val uploadStatusReason: String? = null,
